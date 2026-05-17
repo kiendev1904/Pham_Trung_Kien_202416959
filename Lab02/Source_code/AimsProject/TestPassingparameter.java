6a33c314008b3fc997eeb7aa72362c230076f137
kiendev1904 <phamtrungkien1904@gmail.com>
2026-05-17 22:20:55 +0700

    Add method swap() can correctly swap the two objects

--- Lab02/Source_code/AimsProject/TestPassingparameter.java
+++ Lab02/Source_code/AimsProject/TestPassingparameter.java
@@ -1,30 +1,17 @@
 public class TestPassingparameter {
 
     public static void main(String[] args) {
-        DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
-        DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
+        DigitalVideoDisc[] dvds = new DigitalVideoDisc[2];
+        dvds[0] = new DigitalVideoDisc("Jungle");
+        dvds[1] = new DigitalVideoDisc("Cinderella");
+        swap(dvds, 0, 1);
+        System.out.println("After swapping: dvds[0] = " + dvds[0].getTitle() + ", dvds[1] = " + dvds[1].getTitle());
 
 
-        swap(jungleDVD, cinderellaDVD);
-        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
-        System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
-
-
-        changeTitle(jungleDVD, cinderellaDVD.getTitle());
-        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
     }
-
-
-    public static void swap(Object o1, Object o2) {
-        Object tmp = o1;
-        o1 = o2;
-        o2 = tmp;
-    }
-
-
-    public static void changeTitle(DigitalVideoDisc dvd, String title) {
-        String oldTitle = dvd.getTitle();
-        dvd.setTitle(title);
-        dvd = new DigitalVideoDisc(oldTitle);
+    public static void swap(DigitalVideoDisc[] arr, int i, int j) {
+        DigitalVideoDisc temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
