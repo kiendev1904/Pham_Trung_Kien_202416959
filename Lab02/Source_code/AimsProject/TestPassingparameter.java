2b5aca1de5b8e6939f01e034fa984845f57567ce
kiendev1904 <phamtrungkien1904@gmail.com>
2026-05-17 22:28:55 +0700

    Done debug

--- Lab02/Source_code/AimsProject/TestPassingparameter.java
+++ Lab02/Source_code/AimsProject/TestPassingparameter.java
@@ -8,10 +8,15 @@ public class TestPassingparameter {
         swap(jungleDVD, cinderellaDVD);
         System.out.println("jungle dvd title: " + jungleDVD.getTitle());
         System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
-
-
         changeTitle(jungleDVD, cinderellaDVD.getTitle());
         System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+        System.out.println("Real swap in Java");
+        DigitalVideoDisc[] dvds = new DigitalVideoDisc[2];
+        dvds[0] = new DigitalVideoDisc("Jungle");
+        dvds[1] = new DigitalVideoDisc("Cinderella");
+        swap(dvds, 0, 1);
+        System.out.println("jungle dvd title: " + dvds[0].getTitle());
+        System.out.println("cinderella dvd title: " + dvds[1].getTitle());
     }
 
 
@@ -26,5 +31,10 @@ public class TestPassingparameter {
         String oldTitle = dvd.getTitle();
         dvd.setTitle(title);
         dvd = new DigitalVideoDisc(oldTitle);
+    }
+    public static void swap(DigitalVideoDisc[] arr, int i, int j) {
+        DigitalVideoDisc temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
