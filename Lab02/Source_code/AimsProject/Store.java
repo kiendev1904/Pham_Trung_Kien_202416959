119e34d10672eca351c999d6ba9fb337632d56b8
kiendev1904 <phamtrungkien1904@gmail.com>
2026-05-17 23:01:23 +0700

    Add Store

---
+++ Lab02/Source_code/AimsProject/Store.java
@@ -0,0 +1,57 @@
+public class Store {
+    // Thuộc tính: mảng lưu các DVD trong cửa hàng
+    private DigitalVideoDisc[] itemsInStore;
+    private int qtyStored;           // số lượng DVD hiện có
+    public static final int MAX_NUMBERS_STORED = 100; // sức chứa tối đa
+
+    // Constructor khởi tạo mảng
+    public Store() {
+        itemsInStore = new DigitalVideoDisc[MAX_NUMBERS_STORED];
+        qtyStored = 0;
+    }
+
+    // Phương thức thêm DVD vào cửa hàng
+    public void addDVD(DigitalVideoDisc dvd) {
+        if (qtyStored < MAX_NUMBERS_STORED) {
+            itemsInStore[qtyStored] = dvd;
+            qtyStored++;
+            System.out.println("DVD \"" + dvd.getTitle() + "\" has been added to the store.");
+        } else {
+            System.out.println("The store is full. Cannot add more DVDs.");
+        }
+    }
+
+    // Phương thức xóa DVD khỏi cửa hàng (dựa trên đối tượng)
+    public void removeDVD(DigitalVideoDisc dvd) {
+        boolean found = false;
+        for (int i = 0; i < qtyStored; i++) {
+            if (itemsInStore[i] == dvd) {
+                // Dịch các phần tử phía sau lên trước
+                for (int j = i; j < qtyStored - 1; j++) {
+                    itemsInStore[j] = itemsInStore[j + 1];
+                }
+                itemsInStore[qtyStored - 1] = null;
+                qtyStored--;
+                found = true;
+                System.out.println("DVD \"" + dvd.getTitle() + "\" has been removed from the store.");
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("DVD \"" + dvd.getTitle() + "\" not found in the store.");
+        }
+    }
+
+    // Phương thức in ra tất cả DVD trong cửa hàng (dùng để test)
+    public void printStore() {
+        System.out.println("*********************** STORE ***********************");
+        if (qtyStored == 0) {
+            System.out.println("The store is empty.");
+        } else {
+            for (int i = 0; i < qtyStored; i++) {
+                System.out.println((i + 1) + ". " + itemsInStore[i].getTitle());
+            }
+        }
+        System.out.println("******************************************************");
+    }
+}
---
+++ Lab02/Source_code/AimsProject/StoreTest.java
@@ -0,0 +1,39 @@
+public class StoreTest {
+    public static void main(String[] args) {
+        // Tạo cửa hàng
+        Store myStore = new Store();
+
+        // Tạo một số DVD mẫu (giả sử lớp DigitalVideoDisc có các constructor phù hợp)
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 121, 24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", "Animation", "Ron Clements", 90, 18.99f);
+        DigitalVideoDisc dvd4 = new DigitalVideoDisc("Jungle Book", "Animation", "Wolfgang Reitherman", 78, 14.95f);
+
+        // Thêm DVD vào store
+        System.out.println("--- Adding DVDs ---");
+        myStore.addDVD(dvd1);
+        myStore.addDVD(dvd2);
+        myStore.addDVD(dvd3);
+        myStore.addDVD(dvd4);
+
+        // In cửa hàng sau khi thêm
+        myStore.printStore();
+
+        // Xóa một DVD
+        System.out.println("\n--- Removing 'Star Wars' ---");
+        myStore.removeDVD(dvd2);
+
+        // In lại cửa hàng sau khi xóa
+        myStore.printStore();
+
+        // Thử xóa một DVD không có trong store
+        DigitalVideoDisc dvd5 = new DigitalVideoDisc("Frozen", "Animation", "Chris Buck", 102, 22.95f);
+        System.out.println("\n--- Trying to remove a DVD not in store ---");
+        myStore.removeDVD(dvd5);
+
+        // Thử thêm DVD khi store đầy (không cần test với MAX=100, nhưng có thể thêm vòng lặp để minh họa)
+        // (nếu cần, có thể tạo thêm DVD và thêm đến khi đầy)
+        System.out.println("\n--- Store status at end ---");
+        myStore.printStore();
+    }
+}
